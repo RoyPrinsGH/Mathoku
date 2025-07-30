e1ee502adbbab1cc3ba687751aebe714e17f105f
RoyPrinsGH <royprinsgm@gmail.com>
2025-07-30 21:56:21 +0200

    Add typeshare.toml and rustfmt formatting

--- mathoku-core/build.rs
+++ mathoku-core/build.rs
@@ -1,12 +1,56 @@
-use std::process::Command;
+use std::{
+    path::PathBuf,
+    process::Command,
+};
+use toml;
+
+struct TypeshareConfig {
+    output_folder: String,
+    name: String,
+}
+
+fn parse_typeshare_config() -> TypeshareConfig {
+    const TYPESHARE_TOML_FILE_NAME: &str = "typeshare.toml";
+    const BUILD_CONFIG_SECTION: &str = "build";
+
+    let typeshare_toml_raw = std::fs::read_to_string(TYPESHARE_TOML_FILE_NAME).expect(&format!("Failed to read {TYPESHARE_TOML_FILE_NAME}"));
+
+    let typeshare_toml = toml::de::from_str::<toml::Value>(&typeshare_toml_raw).expect(&format!("Failed to parse {TYPESHARE_TOML_FILE_NAME}"));
+
+    let build_config = typeshare_toml
+        .get(BUILD_CONFIG_SECTION)
+        .expect(&format!("{TYPESHARE_TOML_FILE_NAME} does not contain a [{BUILD_CONFIG_SECTION}] section"));
+
+    let get_value = |key: &str| {
+        build_config
+            .get(key)
+            .expect(&format!(
+                "{key} not specified in {TYPESHARE_TOML_FILE_NAME} under [{BUILD_CONFIG_SECTION}] section"
+            ))
+            .to_string()
+    };
+
+    let output_folder = get_value("output_folder");
+    let name = get_value("name");
+
+    TypeshareConfig { output_folder, name }
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=src/models");
 
-    let output_file = "../mathoku_ui/src/types/backend.ts";
+    let config = parse_typeshare_config();
+
+    let output_file = PathBuf::from(&config.output_folder).join(format!("{}.ts", config.name));
 
     let output = Command::new("typeshare")
-        .args(["--lang", "typescript", "--output-file", output_file, "."])
+        .args([
+            "--lang",
+            "typescript",
+            "--output-file",
+            output_file.to_str().expect("Invalid output file path"),
+            ".",
+        ])
         .output()
         .expect("Failed to execute typeshare command");
 
@@ -19,7 +63,7 @@ fn main() {
         );
     }
 
-    println!("Successfully generated TypeScript types to {}", output_file);
+    println!("Successfully generated TypeScript types to {output_file:?}");
 
     println!("cargo:rerun-if-changed=build.rs");
 }
--- mathoku-core/src/models/user.rs
+++ mathoku-core/src/models/user.rs
@@ -1,4 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    Deserialize,
+    Serialize,
+};
 
 // Just a simple user model for demonstration purposes
 #[typeshare::typeshare]
